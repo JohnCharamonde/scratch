a54070e88745761273eaceb55479d001c491a0bf
JohnCharamonde <john.charamonde@gmail.com>
2020-09-07 10:48:28 -0400

    WinningNumbers rendering with clippath

--- src/Components/Scratchy.jsx
+++ src/Components/Scratchy.jsx
@@ -4,16 +4,26 @@ import WinningNumbers from './WinningNumbers.jsx';
 import YourNumbers from './YourNumbers.jsx';
 import PersonalMessage from './PersonalMessage.jsx'
 
-function Scratchy() {
-  return (
-    <div style={{"display":"flex", "justifyContent":"Center"}}>
-      <img src={skin} alt="Skin" style={{"height": "60%", "width":"60%", "marginTop": "1%", "zIndex":"100"}}/>
-        <WinningNumbers />
-        <YourNumbers />
-        <PersonalMessage />
-      <div style={{"display":"flex", "backgroundColor": "rgb(25, 24, 27)", "width":"27.4%", "marginTop":"1%"}}></div>
-    </div>
-  )
-}
+class Scratchy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winningNumbers: [{number: 40, text: 'FRTY', shape:'c'}, {number: 30, text: 'TRTY', shape:'c'},{number: 20, text: 'TNTY', shape:'c'},{number: 9, text: 'NINE', shape:'c'},{number: 7, text: 'SEVN', shape:'c'}]
+    }
+
+  }
+
+  render() {
+    return (
+      <div style={{"display":"flex", "justifyContent":"Center"}}>
+        <img src={skin} alt="Skin" style={{"height": "60%", "width":"60%", "marginTop": "1%", "zIndex":"100"}}/>
+          <WinningNumbers winningNumbers={this.state.winningNumbers}/>
+          <YourNumbers />
+          <PersonalMessage />
+        <div style={{"display":"flex", "backgroundColor": "rgb(25, 24, 27)", "width":"27.4%", "marginTop":"1%"}}></div>
+      </div>
+    )
+  }
+};
 
 export default Scratchy;
--- src/Components/WinningNumbers.jsx
+++ src/Components/WinningNumbers.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import WinningNumber from './WinningNumber.jsx'
-import Background from './contours.png'
+import WinningNumber from './WinningNumber.jsx';
+import Background from './contours.png';
 
-function WinningNumbers() {
+function WinningNumbers(props) {
   return (
     <div style={{"display":"flex","position": "absolute", "backgroundColor": "beige", "backgroundImage": "url(" + Background + ")", "height": "10.1%", "width":"26.5%", "left":"37.8%", "top":"20.9%"}}>
-      <WinningNumber number={17} text={'SVNTN'} shape={'a'}/>
-      <WinningNumber number={7} text={'SVN'} shape={'b'}/>
-      <WinningNumber number={9} text={'NINE'} shape={'c'}/>
-      <WinningNumber number={40} text={'FRTY'} shape={'d'}/>
-      <WinningNumber number={24} text={'TWYFR'} shape={'e'}/>
+      {props.winningNumbers.map(winningNumber => 
+        <WinningNumber number={winningNumber.number} text={winningNumber.text} shape={winningNumber.shape}/>
+      )}
     </div>
   )
 }
-export default WinningNumbers
+
+export default WinningNumbers;
