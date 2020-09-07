648769036be90696bac5f88ab65d52a4e8ec9927
JohnCharamonde <john.charamonde@gmail.com>
2020-09-06 23:41:10 -0400

    Working on winning number formatting

--- src/Components/WinningNumber.jsx
+++ src/Components/WinningNumber.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Background from './contours.png'
 function WinningNumber(props) {
+  const shapes = {
+    a: "polygon(24% 16%, 16% 25%, 19% 22%, 18% 31%, 20% 29%, 18% 37%, 20% 33%, 18% 43%, 21% 42%, 20% 50%, 21% 50%, 19% 60%, 20% 60%, 20% 67%, 23% 66%, 21% 74%, 23% 72%, 24% 77%, 31% 78%, 34% 77%, 36% 79%, 38% 80%, 50% 77%, 53% 78%, 56% 79%, 59% 79%, 62% 81%, 68% 78%, 80% 76%, 86% 53%, 83% 49%, 84% 44%, 80% 44%, 82% 37%, 84% 32%, 83% 28%, 80% 23%, 80% 24%, 78% 18%, 70% 19%, 70% 17%, 64% 18%, 61% 16%, 54% 19%, 55% 17%, 48% 19%, 49% 17%, 39% 20%, 35% 18%, 32% 18%)",
+    b: "polygon(10% 28%, 11% 32%, 9% 34%, 11% 38%, 10% 41%, 12% 47%, 10% 51%, 9% 56%, 8% 63%, 11% 65%, 9% 70%, 9% 76%, 26% 73%, 34% 73%, 41% 73%, 50% 73%, 59% 71%, 70% 66%, 79% 68%, 91% 68%, 97% 63%, 93% 60%, 96% 55%, 94% 48%, 94% 43%, 96% 38%, 96% 32%, 93% 27%, 95% 20%, 89% 16%, 78% 16%, 72% 14%, 64% 13%, 55% 13%, 45% 17%, 35% 17%, 23% 18%, 18% 18%, 12% 21%, 6% 23%)",
+    c: "polygon(69% 72%, 64% 85%, 78% 36%, 70% 49%, 92% 8%, 83% 8%, 72% 21%, 70% 15%, 63% 13%, 62% 16%, 49% 20%, 39% 35%, 47% 20%, 45% 23%, 38% 34%, 36% 37%, 32% 36%, 24% 35%, 21% 45%, 20% 56%, 23% 52%, 27% 43%, 14% 80%, 26% 83%, 34% 80%, 37% 74%, 34% 89%, 38% 94%, 50% 76%, 40% 95%, 54% 85%, 61% 87%, 71% 87%)",
+    d: "polygon(25% 13%, 30% 12%, 31% 15%, 29% 18%, 29% 18%, 38% 17%, 44% 6%, 38% 24%, 36% 33%, 57% 23%, 64% 12%, 78% 9%, 78% 26%, 72% 36%, 59% 58%, 40% 73%, 59% 63%, 65% 75%, 63% 82%, 59% 84%, 53% 87%, 42% 88%, 25% 86%, 22% 100%, 15% 90%, 15% 68%, 12% 77%, 16% 49%, 10% 59%, 10% 68%, 13% 26%, 10% 41%, 6% 52%, 44% 24%, 20% 17%, 23% 33%, 16% 41%, 34% 30%",
+    e: "polygon(24% 24%, 39% 8%, 17% 40%, 28% 30%, 14% 52%, 2% 75%, 14% 61%, 4% 94%, 7% 97%, 13% 98%, 19% 82%, 16% 95%, 59% 99%, 74% 79%, 64% 93%, 88% 66%, 83% 44%, 100% 7%, 97% 1%, 68% 9%, 45% 5%)"
+  }
   // if(props.number !== 7 || props.number !== 9) {
   //   return (
   //     <div style={{"display":"flex", "backgroundColor": "pink", "backgroundImage": "url(" + Background + ")","height": "100%", "width": "25%", "zIndex":"99","fontFamily":"Apercu,Lucida Grande,sans-serif", "fontWeight":"bolder", "alignItems": "center", "justifyContent": "center"}}>
@@ -12,7 +19,7 @@ function WinningNumber(props) {
   // )
   // }
   return (
-      <div style={{"display":"flex", "backgroundColor": "aqua", "backgroundImage": "url(" + Background + ")","height": "100%", "width": "25%", "zIndex":"101","fontFamily":"Helvetica", "fontWeight":"bolder", "alignItems": "center", "justifyContent": "center", "white":"black", "clipPath":"polygon(4% 22%, 16% 57%, 10% 47%, 3% 36%, 53% 5%, 5% 57%, 16% 17%, 51% 37%, 76% 35%, 83% 40%, 66% 79%, 40% 74%, 26% 80%, 30% 77%, 35% 15%, 70% 13%, 65% 32%, 16% 88%, 11% 65%)"}}>
+      <div style={{"display":"flex", "backgroundColor": "aqua", "backgroundImage": "url(" + Background + ")","height": "100%", "width": "25%", "zIndex":"101","fontFamily":"Helvetica", "fontWeight":"bolder", "alignItems": "center", "justifyContent": "center", "white":"black", "clipPath":shapes[props.shape]}}>
       <div style={{"display": "flex", "flexDirection": "column", "alignItems": "center", "justifyContent":"center", "height":"90%", "width":"90%", "border":".1px solid pink"}}>
       <div style={{"display":"flex", "alignItems": "center", "justifyContent":"center", "height":"30%", "width":"100%", "fontSize":"160%"}}>{props.number}</div>
       <div style={{"display":"flex", "alignItems": "center", "justifyContent":"center", "height":"20%", "width":"100%", "fontSize":"10%"}}>{props.text}</div>
--- src/Components/WinningNumbers.jsx
+++ src/Components/WinningNumbers.jsx
@@ -5,11 +5,11 @@ import Background from './contours.png'
 function WinningNumbers() {
   return (
     <div style={{"display":"flex","position": "absolute", "backgroundColor": "beige", "backgroundImage": "url(" + Background + ")", "height": "10.1%", "width":"26.5%", "left":"37.8%", "top":"20.9%"}}>
-      <WinningNumber number={7} text={'SVN'}/>
-      <WinningNumber number={7} text={'SVN'}/>
-      <WinningNumber number={9} text={'NINE'}/>
-      <WinningNumber number={7} text={'SVN'}/>
-      <WinningNumber number={7} text={'SVN'}/>
+      <WinningNumber number={17} text={'SVNTN'} shape={'a'}/>
+      <WinningNumber number={7} text={'SVN'} shape={'b'}/>
+      <WinningNumber number={9} text={'NINE'} shape={'c'}/>
+      <WinningNumber number={40} text={'FRTY'} shape={'d'}/>
+      <WinningNumber number={24} text={'TWYFR'} shape={'e'}/>
     </div>
   )
 }
