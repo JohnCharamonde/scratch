fcd40c2b016a493e81cb45e8d3398c20301f6bdc
JohnCharamonde <john.charamonde@gmail.com>
2020-10-01 17:25:38 -0400

    PrizeShowcase rendering only when active

--- src/Components/Scratchy/PrizeShowcase/PrizeShowcase.jsx
+++ src/Components/Scratchy/PrizeShowcase/PrizeShowcase.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 function PrizeShowcase(props) {
   return (
     <div>
-      
+    {props.isActive ? 
+      <div
+      style={{
+        "position":"absolute",
+        "left":"30%",
+        "top":"15%",
+        "backgroundColor":"orange",
+        "height":"30%",
+        "width":"25%",
+        "zIndex":"500"
+      }}>
+      hey what's up!
+      </div>
+      :
+      ''
+    }
     </div>
   )
 }
